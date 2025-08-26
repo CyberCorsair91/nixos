cbf3bfd96c1e8138165a685f7780ca531c30746c
CyberCorsair91 <CyberCorsair91@users.noreply.github.com>
2025-08-26 15:10:25 +0200

    feat(modules): add bootloader.nix and locales.nix

--- configuration.nix
+++ configuration.nix
@@ -9,11 +9,9 @@
       ./modules/packages.nix
       ./modules/bluetooth.nix
       ./modules/nvidia.nix
+      ./modules/bootloader.nix
+      ./modules/locales.nix
     ];
-
-  # Bootloader
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
 
   # Use latest kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
@@ -23,23 +21,6 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
-
-  # Timezone
-  time.timeZone = "Europe/Bratislava";
-
-  # Locale settings
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "sk_SK.UTF-8";
-    LC_IDENTIFICATION = "sk_SK.UTF-8";
-    LC_MEASUREMENT = "sk_SK.UTF-8";
-    LC_MONETARY = "sk_SK.UTF-8";
-    LC_NAME = "sk_SK.UTF-8";
-    LC_NUMERIC = "sk_SK.UTF-8";
-    LC_PAPER = "sk_SK.UTF-8";
-    LC_TELEPHONE = "sk_SK.UTF-8";
-    LC_TIME = "sk_SK.UTF-8";
-  };
 
   # Enable X11 and GNOME
   services.xserver.enable = true;
